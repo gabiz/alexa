cebe09170ace9246fac8965067c09d23fdccb108
ed bond <celpa.firl@gmail.com>
2017-05-03 03:04:20 -0500

    add ability to play audio and have an agnostic handler

--- lib/response/response.ex
+++ lib/response/response.ex
@@ -19,10 +19,10 @@ defmodule PhoenixAlexa.Response do
   end
 
   def set_output_speech(response, %TextOutputSpeech{} = outputspeech) do
-    %Response{response | response: (response.response |> Map.put(:outputSpeech, outputspeech))}     
+    %Response{response | response: (response.response |> Map.put(:outputSpeech, outputspeech))}
   end
   def set_output_speech(response, %SsmlOutputSpeech{} = outputspeech) do
-    %Response{response | response: (response.response |> Map.put(:outputSpeech, outputspeech))}     
+    %Response{response | response: (response.response |> Map.put(:outputSpeech, outputspeech))}
   end
 
   def set_card(response, %SimpleCard{} = card) do
@@ -49,7 +49,7 @@ defmodule PhoenixAlexa.Response do
   # Card helpers
 
   def set_title(%SimpleCard{} = card, title) do
-    SimpleCard.set_title(card, title)    
+    SimpleCard.set_title(card, title)
   end
   def set_title(%StandardCard{} = card, title) do
     StandardCard.set_title(card, title)
@@ -76,5 +76,50 @@ defmodule PhoenixAlexa.Response do
   def set_large_image_url(%StandardCard{} = card, image_url) do
     StandardCard.set_large_image_url(card, image_url)
   end
+
+  #Audio Helpers
+
+  #Audio helpers
+  def put_stop_audio_directive(response) do
+    directive = %{
+      :type => "AudioPlayer.Stop"
+     }
+     put_directive(response,[directive])
+  end
+  def put_replace_audio_directive(response,url,token,offset) do
+    directive = %{
+      :type => "AudioPlayer.Play",
+      :playBehavior => "REPLACE_ALL",
+      :audioItem => %{
+        :stream => %{
+            :url => url,
+            :token => token,
+            :offsetInMilliseconds => offset
+        }
+      }
+     }
+     put_directive(response,[directive])
+  end
+  def put_enqueue_audio_directive(response,url,token,previous_token,offset) do
+    directive = %{
+      :type => "AudioPlayer.Play",
+      :playBehavior => "ENQUEUE",
+      :audioItem => %{
+        :stream => %{
+            :url => url,
+            :token => token,
+            :offsetInMilliseconds => offset,
+            :expectedPreviousToken => previous_token
+        }
+      }
+     }
+     put_directive(response,[directive])
+  end
+
+  # put directives
+  def put_directive(response,directives) do
+    %Response{response | response: (response.response |> Map.put(:directives, directives))}
+  end
+
 
 end
